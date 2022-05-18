8d41707f4ff89123e194f1b0ee2404e82ee94729
Mikael Sauriol <mikaelsauriol5@hotmail.com>
2022-05-18 17:00:01 -0400

    Ratin g

--- views/Rating.js
+++ views/Rating.js
@@ -6,13 +6,12 @@ import {
   Button,
   Alert,
   TouchableHighlight,
-
 } from "react-native";
 import styles from "./styles";
 import Slider from "@react-native-community/slider";
 import { doc, setDoc, Timestamp } from "firebase/firestore";
-import { auth, db } from '../firebase';
-import moment from 'moment';
+import { auth, db } from "../firebase";
+import moment from "moment";
 
 //auth.currentUser?.email
 
@@ -40,11 +39,9 @@ const Rating = () => {
             minimumTrackTintColor="white"
             maximumTrackTintColor="white"
             value={data1}
-            onValueChange={
-              (sliderValue1) => setSliderData1(sliderValue1)
-            }
+            onValueChange={(sliderValue1) => setSliderData1(sliderValue1)}
           />
-          <Text style={{ color: "white", marginLeft: 25, fontSize: 30, }}>
+          <Text style={{ color: "white", marginLeft: 25, fontSize: 30 }}>
             {data1}
           </Text>
         </View>
@@ -60,11 +57,9 @@ const Rating = () => {
             minimumTrackTintColor="white"
             maximumTrackTintColor="white"
             value={data}
-            onValueChange={
-              (sliderValue) => setSliderData(sliderValue)
-            }
+            onValueChange={(sliderValue) => setSliderData(sliderValue)}
           />
-          <Text style={{ color: "white", marginLeft: 25, fontSize: 30, }}>
+          <Text style={{ color: "white", marginLeft: 25, fontSize: 30 }}>
             {data}
           </Text>
         </View>
@@ -75,26 +70,38 @@ const Rating = () => {
           width: 160,
           borderRadius: 10,
           backgroundColor: "grey",
-          marginTop: 70
-        }}>
+          marginTop: 70,
+        }}
+      >
         <Button
           onPress={() =>
-            setDoc(doc(db, "Satisfaction", moment().format('lll')), {
+            setDoc(doc(db, "Satisfaction", (auth.currentUser?.email + ' ' + moment().format("YYYY-MM-DD"))), {
               user: auth.currentUser?.uid,
               user_email: auth.currentUser?.email,
-              date: moment().format('lll'),
+              date: moment().format("lll"),
               Comfort: data1,
-              Satisfaction: data
-            }).then(Alert.alert('Bien envoye. Merci pour ton temps!'))
+              Satisfaction: data,
+            }).then(Alert.alert("Bien envoye. Merci pour ton temps!"))
           }
           title="Envoyer"
           color="white"
         />
+        {/* <Button
+          onPress={() =>
+            setDoc(doc(db, "Satisfaction", moment().format("lll")), {
+              user: auth.currentUser?.uid,
+              user_email: auth.currentUser?.email,
+              date: moment().format("lll"),
+              Comfort: data1,
+              Satisfaction: data,
+            }).then(Alert.alert("Bien envoye. Merci pour ton temps!"))
+          }
+          title="Envoyer"
+          color="white"
+        /> */}
       </TouchableHighlight>
     </View>
   );
 };
 
 export default Rating;
-
-
